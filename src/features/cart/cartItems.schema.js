873add5c02ebce5575ebb811f7b663a989f52c91
Shreya S <ssshreya2000@gmail.com>
2024-05-30 06:04:31 +0530

    message

--- src/features/cart/cartItems.schema.js
+++ src/features/cart/cartItems.schema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const productSchema = new mongoose.Schema({
+export const cartSchema = new mongoose.Schema({
   productID: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
   userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   quantity: Number,
